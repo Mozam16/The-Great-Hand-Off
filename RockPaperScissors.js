5c4d933f576812ff188610e458c419220479f024
Mozam16 <hameynah2005@gmail.com>
2026-05-22 15:36:16 +0100

    Updated changes

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,32 +1,46 @@
-const play =(playerchoice)=>{
+const play =(playerchoice, button)=>{
+    let emoji = document.createElement("span")
+    emoji.innerHTML = playerchoice
+    emoji.classList.add("float")
+    
+    button.appendChild(emoji)
+
+
+
     let computerAnswer = Math.floor(Math.random() *3)
 
     let computerChoice = ""
 
     if (computerAnswer == 0 ) {
-        computerChoice = "rock"
+        computerChoice = "✊"
         
     } else if (computerAnswer == 1) {
-        computerChoice = "paper"
+        computerChoice = "✋"
     } else {
-        computerChoice = "scissors"
+        computerChoice = "✌"
     }
 
     let result = ""
-    
-    if (playerchoice == computerChoice ) {
-        result = "It's a DRAW!!!🤝🏾"
-    } else if (playerchoice == "rock" && computerChoice == "paper") {
-        result = "Paper beats Rock, YOU LOSE!!!😢"
-    }else if (playerchoice == "paper" && computerChoice == "scissors"){
-        result = "Scissors beats Paper, YOU LOSE😢"
-    }else if (playerchoice == "scissors" && computerChoice == "rock"){
-        result = "Rock beats Scissors, YOU LOSE😢"
-    }else{
-        result = " YOU WIN!!! 🎊"
-    }
 
-    document.querySelector(".result").innerHTML = result;
+    setTimeout(() => {
+        
+        if (playerchoice == computerChoice ) {
+            result = "It's a DRAW!!!🤝🏾"
+        } else if (playerchoice == "✊" && computerChoice == "✋") {
+            result = "Paper beats Rock, YOU LOSE!!!😢"
+        }else if (playerchoice == "✋" && computerChoice == "✌"){
+            result = "Scissors beats Paper, YOU LOSE😢"
+        }else if (playerchoice == "✌" && computerChoice == "✊"){
+            result = "Rock beats Scissors, YOU LOSE😢"
+        }else{
+            result = " YOU WIN!!! 🎊"
+        }
+
+        document.querySelector(".result").innerHTML = result;
+    }, 1000);
+    
+
+
     document.querySelector(".computerChoice").innerHTML = computerChoice;
     document.querySelector(".playerChoice").innerHTML = playerchoice;
 }
